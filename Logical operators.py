d162e256da5c9b7ee7c14f19d44c86774ed34d1f
DEBISHREENAYAK <57474704+DEBISHREENAYAK@users.noreply.github.com>
2021-07-20 14:38:58 +0530

    Add files via upload

---
+++ Logical operators.py
@@ -0,0 +1,17 @@
+#logical 'AND'(&)
+
+a=3
+b=2
+c=a&b
+print(c)
+
+#logical 'OR'()
+x=2
+y=3
+z=x or y
+print(z)
+
+#logical 'nor'()
+p=3
+q=4
+r=p 
---
+++ arithmetic operators.py
@@ -0,0 +1,17 @@
+a=10
+b=20
+
+c=a*b
+print(c)
+
+d=a/b
+print(d)
+
+e=a%b
+print(e)
+
+f=a+b
+print(f)
+
+g=a-b
+print(g)
